85df0481e531918f3f550ce433f6dd0f234303c4
joyAct <joyleeact@gmail.com>
2020-12-06 16:45:53 +0900

    20. useState를 useReducer로 변환 1

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,25 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      throw new Error('Unhandled action');
+  }
+}
 
 function Counter() {
-  const [number, setNumber] = useState(0);
+  const [number, dispatch] = useReducer(reducer, 0);
 
   const increase = () => {
-    setNumber((prevNumber) => prevNumber + 1);
+    dispatch({ type: 'INCREMENT' });
   };
 
   const decrease = () => {
-    setNumber((prevNumber) => prevNumber - 1);
+    dispatch({ type: 'DECREMENT' });
   };
 
   return (
