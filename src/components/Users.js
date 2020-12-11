02c4d397989e7c525644cd0b55a51776cd561e1e
joyAct <joyleeact@gmail.com>
2020-12-11 14:15:55 +0900

    API 연동의 기본 - try & catch문으로 API 불러오기

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Users() {
   const [users, setUsers] = useState(null);
@@ -6,20 +7,41 @@ function Users() {
   const [error, setError] = useState(null);
 
   // 렌더링 시
-  const fetchUsers = async () => {};
+  const fetchUsers = async () => {
+    try {
+      setUsers(null); // 초기화
+      setError(null); // 초기화
+      setLoading(true); // 로딩 시작
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users/'
+      );
+      setUsers(response.data); // 결과값
+    } catch (e) {
+      console.log(e.response.status); // 에러 종류(404)
+      setError(e);
+    }
+    setLoading(false); // 로딩 완료
+  };
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!users) return null;
+
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>
-          {user.username} ({user.name})
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchUsers}>Reloading</button>
+    </>
   );
 }
 
