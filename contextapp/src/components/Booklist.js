dbe2f14ebc1fd323b323ae3dcfe546ddeedf85ae
Joy Lee <joylee.dev@gmail.com>
2021-01-30 15:44:25 +0900

    React Context - Accessing a Context by useContext

--- contextapp/src/components/Booklist.js
+++ contextapp/src/components/Booklist.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 function Booklist() {
+  const context = useContext(ThemeContext);
+  const { isLightTheme, light, dark } = context;
+  const theme = isLightTheme ? light : dark;
+
   return (
-    <ThemeContext.Consumer>
-      {(context) => {
-        const { isLightTheme, light, dark } = context;
-        const theme = isLightTheme ? light : dark;
-        return (
-          <div
-            className="book-list"
-            style={{ color: theme.syntax, background: theme.bg }}
-          >
-            <ul>
-              <li style={{ background: theme.ui }}>the way of kings</li>
-              <li style={{ background: theme.ui }}>the name of the wind</li>
-              <li style={{ background: theme.ui }}>the final empire</li>
-            </ul>
-          </div>
-        );
-      }}
-    </ThemeContext.Consumer>
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>the way of kings</li>
+        <li style={{ background: theme.ui }}>the name of the wind</li>
+        <li style={{ background: theme.ui }}>the final empire</li>
+      </ul>
+    </div>
   );
 }
 
