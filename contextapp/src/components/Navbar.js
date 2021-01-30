e5d78b4789671c59ad6645d4448a93525a7943ac
Joy Lee <joylee.dev@gmail.com>
2021-01-30 16:59:33 +0900

    React Context - Creating Multiple Contexts

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -1,14 +1,22 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 function Navbar() {
-  const context = useContext(ThemeContext);
-  const { isLight, light, dark } = context;
+  const themeContext = useContext(ThemeContext);
+  const authContext = useContext(AuthContext);
+
+  const { isLight, light, dark } = themeContext;
+  const { isAuth, toggleAuth } = authContext;
+
   const theme = isLight ? light : dark;
 
   return (
     <nav style={{ background: theme.ui, color: theme.syntax }}>
       <h1>Context App</h1>
+      <span onClick={() => toggleAuth()}>
+        {isAuth ? 'Logged In' : 'Logged Out'}
+      </span>
       <ul>
         <li>Home</li>
         <li>About</li>
---
+++ contextapp/src/contexts/AuthContext.js
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from 'react';
+
+export const AuthContext = createContext();
+
+function AuthContextProvider({ children }) {
+  const [isAuth, setIsAuth] = useState(false);
+  const toggleAuth = () => {
+    setIsAuth(!isAuth);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuth, toggleAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthContextProvider;
