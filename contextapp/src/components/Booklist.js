2ae9255c7ab6744969ea90092b60519e822e55c0
Joy Lee <joylee.dev@gmail.com>
2021-01-30 15:43:15 +0900

    React Context - Accessing a Context by Consumer

--- contextapp/src/components/Booklist.js
+++ contextapp/src/components/Booklist.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 function Booklist() {
   return (
-    <div className="book-list">
-      <ul>
-        <li>the way of kings</li>
-        <li>the name of the wind</li>
-        <li>the final empire</li>
-      </ul>
-    </div>
+    <ThemeContext.Consumer>
+      {(context) => {
+        const { isLightTheme, light, dark } = context;
+        const theme = isLightTheme ? light : dark;
+        return (
+          <div
+            className="book-list"
+            style={{ color: theme.syntax, background: theme.bg }}
+          >
+            <ul>
+              <li style={{ background: theme.ui }}>the way of kings</li>
+              <li style={{ background: theme.ui }}>the name of the wind</li>
+              <li style={{ background: theme.ui }}>the final empire</li>
+            </ul>
+          </div>
+        );
+      }}
+    </ThemeContext.Consumer>
   );
 }
 
