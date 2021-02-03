862156420999261680f2c8ba6dbfbd7f558b3350
Joy Lee <joylee.dev@gmail.com>
2021-02-03 14:16:25 +0900

    Typescript & React - useState 상태 관리

--- tutorial-react-ts/src/App.tsx
+++ tutorial-react-ts/src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import Greetings from './Greetings';
+import React from 'react';
+import Greetings from './components/Greetings';
+import Counter from './components/Counter';
 
 const App: React.FC = () => {
   const onClick = (name: string) => {
     console.log(name);
-  }
+  };
 
-  return <Greetings name="JOY" onClick={onClick}/>;
-}
+  return (
+    <>
+      <Greetings name="JOY" onClick={onClick} />;
+      <Counter />
+    </>
+  );
+};
 
 export default App;
--- tutorial-react-ts/src/components/Greetings.tsx
+++ tutorial-react-ts/src/components/Greetings.tsx
@@ -1,30 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 type GreetingsProps = {
   name: string;
   mark?: string; // default 값이 부여된 경우 '?' 처리
   onClick: (name: string) => void; // 아무것도 리턴하지 않는 함수
-}
+};
 
-
-// const Greetings: React.FC<GreetingsProps> = () => {} 
-function Greetings({ name, mark, onClick}: GreetingsProps) {
+// const Greetings: React.FC<GreetingsProps> = () => {}
+function Greetings({ name, mark, onClick }: GreetingsProps) {
   const handleClick = () => onClick(name);
 
   return (
     <>
-      <div>Hello, {name} {mark}</div>
+      <div>
+        Hello, {name} {mark}
+      </div>
       <div>
         <button onClick={handleClick}>Click</button>
       </div>
     </>
-  )
+  );
 }
 
 // React.FC 에서는 defaultProps 작동 x
 Greetings.defaultProps = {
-  mark: '!'
-}
-
+  mark: '!',
+};
 
 export default Greetings;
